38ef02f032dd27100a52d0f20742f68c207732f1
fabio <fabio.meihua08@gmail.com>
2020-08-10 16:33:45 -0300

    Criação da Base de Dados

--- src/main/java/com/fabiomatos/cursomc/domain/Categoria.java
+++ src/main/java/com/fabiomatos/cursomc/domain/Categoria.java
@@ -2,12 +2,22 @@ package com.fabiomatos.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+	
+	
 	private String nome;
 	
 	public Categoria() {
